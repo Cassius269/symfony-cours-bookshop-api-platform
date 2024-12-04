f21d3d0a51f6d912183fa344c4eef7ae1422dd9f
cassius269 <fahamygaston@gmail.com>
2024-12-04 22:25:21 +0100

    mise en place de la pagination des ressources

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -7,6 +7,7 @@ use ApiPlatform\Metadata\Put;
 use ApiPlatform\Metadata\Post;
 use Doctrine\DBAL\Types\Types;
 use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Delete;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ArticleRepository;
@@ -17,10 +18,24 @@ use ApiPlatform\Metadata\GetCollection;
 #[ApiResource(
     operations: [
         new Get(), // rendre accessible une ressource grâce à son ID
-        new GetCollection(), // rendre accessible l'ensemble des ressources
+
+        new GetCollection( // rendre accessible l'ensemble des ressources
+            paginationEnabled: true, // pagination de la data activée par défaut
+            paginationItemsPerPage: 20,  // définir le nombre de ressources articles à afficher par page, 
+            paginationClientEnabled: true, // donner la possibilité au client de choisir l'activation de la pagination
+            paginationClientItemsPerPage: true, // donner la possibilité au client de choisir le nombre d'objets ressources par page, 
+            uriTemplate: '/getarticles', // création d'une route personnalisé
+            name: 'getArticles' // donner à la route un nom personnalisé
+        ),
+        new GetCollection(
+            paginationEnabled: false, // pagination désactivée
+            uriTemplate: '/getarticles2', // création d'une route personnalisé
+            name: 'getArticles2' // donner à la route un nom personnalisé
+        ),
         new Post(), // créer une nouvelle ressource
         new Put(), // mettre à jour une ressource complètement
-        new Patch() // mettre à jour une ressource en particulière de façon partielle
+        new Patch(), // mettre à jour une ressource en particulière de façon partielle
+        new Delete() // supprimer une ressource Article
     ]
 )]
 class Article
