9afd17a945275d7689b28507456871e2caa39f49
cassius269 <fahamygaston@gmail.com>
2024-12-13 21:49:15 +0100

    traitement des données renvoyées sans modification des données présentes en base données: titre des articles mis en majuscules à l'affichage

--- src/State/CustomGetCollectionProvider.php
+++ src/State/CustomGetCollectionProvider.php
@@ -5,13 +5,23 @@ namespace App\State;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
 use App\Repository\ArticleRepository;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class CustomGetCollectionProvider implements ProviderInterface
 {
-    public function __construct(private ArticleRepository $articleRepository) {}
+    public function __construct(private ArticleRepository $articleRepository, #[Autowire(service: 'api_platform.doctrine.orm.state.collection_provider')] private ProviderInterface $providerInterface) {}
+
+
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
-        return $this->articleRepository->findOneByTitle("L'Importance de l'Hydratation");
+        // Récuperer les données
+        $data = $this->providerInterface->provide($operation, $uriVariables, $context);
+
+        // Mettre en majuscule chaque titre de chaque ressource Article recupéréee
+        foreach ($data as $key => $value) {
+            $value->setTitle(strtoupper($value->getTitle()));
+        }
+        return $data;
     }
 }
